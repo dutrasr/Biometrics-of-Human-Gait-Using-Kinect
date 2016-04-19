a19987ca7b8ca178de4d3a4b8f688b0bb7777c33
Rafael Dutra <dutrasr@gmail.com>
2016-04-19 03:28:11 -0300

    make for the GenerateInfo files

---
+++ generateInfo.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# @Author: dutra
+# @Date:   2015-10-13 16:34:40
+# @Last Modified by:   dutra
+# @Last Modified time: 2015-11-16 01:24:46
+
+# First: python3 path/to/.py('test.py') args('anth' 'gaitAttrib' 'meanStd' 'filtered')
+# args are optional
+
+python3 GenerateInfo/test.py $*
